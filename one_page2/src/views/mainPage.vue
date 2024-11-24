ffdab68726e1a70fb7df0f24f43e0c8de9bce3e6
Aleksandrs Sloka <aleksandrs.sloka@vases.lv>
2024-11-24 15:56:09 +0200

    add colapse menu
    Signed-off-by: Aleksandrs Sloka <aleksandrs.sloka@vases.lv>

--- one_page2/src/views/mainPage.vue
+++ one_page2/src/views/mainPage.vue
@@ -1,5 +1,18 @@
 <template>
-    <div id="map" style="width: 100%; height: 100vh;"></div>
+    <div>
+        <b-button v-b-toggle.collapse-1 variant="primary">Toggle Collapse</b-button>
+        <b-collapse id="collapse-1" class="mt-2">
+            <b-card>
+                <p class="card-text">Collapse contents Here</p>
+                <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+                <b-collapse id="collapse-1-inner" class="mt-2">
+                    <b-card>Hello!</b-card>
+                </b-collapse>
+            </b-card>
+        </b-collapse>
+
+        <div id="map" style="width: 100%; height: 100vh;"></div>
+    </div>
 </template>
 
 <script>
@@ -16,9 +29,11 @@ export default {
         initMap() {
             const map = new maplibregl.Map({
                 container: 'map', // container id
-                style: 'https://demotiles.maplibre.org/style.json', // style URL
+                style: 'https://api.maptiler.com/maps/outdoor-v2/style.json?key=SNBPG5wFM6FhUXbN07ua', // style URL
                 center: [24.562966769607783, 56.798003718105406], // starting position [lng, lat]
-                zoom: 6.743059121016559 // starting zoom
+                zoom: 6.743059121016559, // starting zoom
+                minZoom: 5.5,
+                maxZoom: 9,
             });
 
             // Получение текущих координат центра карты
