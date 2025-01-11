01ff01ab47aec41a9cc300ccd458c269c2492785
Aleksandrs Sloka <aleksandrs.sloka@vases.lv>
2025-01-11 14:57:37 +0200

    create data save in database
    Signed-off-by: Aleksandrs Sloka <aleksandrs.sloka@vases.lv>

--- django/project/first_part/models.py
+++ django/project/first_part/models.py
@@ -1,38 +1,17 @@
 from django.contrib.gis.db import models
-# from django.db import models 
-# from django.core.validators import MinValueValidator, MaxValueValidator
-
-
-# class Anime(models.Model):
-#     name = models.CharField(max_length=255)
-#     episodeCount = models.IntegerField(default=12)
-#     grade = models.IntegerField(default = 0, validators=[MinValueValidator(0), MaxValueValidator(100)])
-#     statusF = models.ForeignKey(to='Status', on_delete=models.SET_NULL, default=1, null=True)
-#     onEpisode = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-
-#     def __str__(self):
-#         return self.name
-
-# class Status(models.Model):
-#     name = models.CharField(max_length=10)
-
-#     def __str__(self):
-#         return self.name  
-
 
 class Place(models.Model):
     place_name = models.CharField(max_length=100)
     time_received = models.DateTimeField()
     plane_distance = models.FloatField(blank=True, null=True)
-    test_field1 = models.FloatField()
     
+    PositionData = models.ForeignKey(PositionData, on_delete=models.DO_NOTHING, related_name="positionData", blank=True)
+
     def __str__(self):
         return f"{self.place_name} ({self.plane_distance} km)"
 
 
 class PositionData(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.DO_NOTHING, related_name="positions", null=True, blank=True)
-
     position = models.PointField(geography=True) 
     altitude = models.FloatField()
     speed = models.FloatField()
@@ -42,11 +21,9 @@ class PositionData(models.Model):
     vepu = models.FloatField(blank=True, null=True)
     hfomr = models.FloatField(blank=True, null=True)
     vfomr = models.FloatField(blank=True, null=True)
-    hex_code = models.CharField(blank=True, null=True)
-    icao_id = models.CharField(max_length=10, unique=True)
+    hex_code = models.CharField(blank=True, unique=True)
+    icao_id = models.CharField(max_length=10)
     call_sign = models.CharField(max_length=20, blank=True, null=True)
-    test_field2 = models.FloatField()
-    test = models.CharField()
 
     
     def __str__(self):
--- django/project/first_part/tasks.py
+++ django/project/first_part/tasks.py
@@ -1,5 +1,48 @@
 from celery import shared_task
+import json
+from .models import PositionData, Place
+from collections import OrderedDict
+from django.contrib.gis.geos import Point
 
 @shared_task
-def process_message_from_rabbitmq(message):
-    print(f"Processing message: {message}")
+def process_message_from_rabbitmq(json_message):
+    print(f"Processing message: {json_message}")
+
+    dict_message = json.loads(json_message)
+    
+    try:
+        orderedDictPlace = OrderedDict()
+        orderedDictPositionData = OrderedDict()
+
+        orderedDictPlace['place_name'] = dict_message['place_Name']
+        orderedDictPlace['time_received'] = dict_message['time_received']
+        orderedDictPlace['plane_distance'] = dict_message['Plane_distance']
+        orderedDictPlace['PositionData'] = None
+
+        orderedDictPositionData['position'] = Point(dict_message['Position_longitude'], dict_message['Position_latitude'])
+        orderedDictPositionData['altitude'] = dict_message['Altitude']
+        orderedDictPositionData['speed'] = dict_message['Speed']
+        orderedDictPositionData['track'] = dict_message['Track']
+        orderedDictPositionData['rc'] = dict_message['Rc']
+        orderedDictPositionData['epu'] = dict_message['EPU']
+        orderedDictPositionData['vepu'] = dict_message['VEPU']
+        orderedDictPositionData['hfomr'] = dict_message['HFOMr']
+        orderedDictPositionData['vfomr'] = dict_message['VFOMr']
+        orderedDictPositionData['hex_code'] = dict_message['HEX']
+        orderedDictPositionData['icao'] = dict_message['ICAO']
+        orderedDictPositionData['call_sign'] = dict_message['Callsign']
+
+        position_data, created = PositionData.objects.get_or_create(
+            hex_code = orderedDictPositionData['hex_code'],
+            defaults = orderedDictPositionData
+        )
+
+        Place.objects.create(
+            place_name = orderedDictPlace['place_name'],
+            time_recieved = orderedDictPlace['time_recieved'],
+            plane_distance = orderedDictPlace['plane_distance'],
+            PositionData = position_data
+        )
+
+    except Exception as e:
+        print(f"Error: {e}")
