69391249efdd3a17738ce36ab04b7d588f863847
Aleksandrs Sloka <aleksandrs.sloka@vases.lv>
2025-01-20 03:45:09 +0200

    data vizualizator
    Signed-off-by: Aleksandrs Sloka <aleksandrs.sloka@vases.lv>

--- message_receiver/receiver.js
+++ message_receiver/receiver.js
@@ -1,8 +1,10 @@
 const amqp = require('amqplib');
 const express = require('express');
+const ws = require('ws');
 const app = express();
+const serverWS = new ws.Server({ port: 8082 });; 
 
-app.use(express.text()); // Для обработки текстового тела запроса
+app.use(express.json()); // Для обработки текстового тела запроса
 
 const RABBITMQ_URL = 'amqp://admin:Password1234@localhost:5672';
 const QUEUE_NAME = 'to_django_data';
@@ -35,24 +37,48 @@ async function sendMessageToQueue(message) {
         }
 
         // Отправляем сообщение в очередь
-        channel.sendToQueue(QUEUE_NAME, Buffer.from(message), {
+        channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(message)), {
             persistent: true, // Сообщение сохраняется на диске
         });
-        console.log(` [x] Sent message to queue '${QUEUE_NAME}':`, message);
+        console.log(` [x] Sent message to queue '${QUEUE_NAME}':`);
     } catch (error) {
         console.error('Failed to send message:', error);
+    }
+}
+
+async function sendMessageToVue(message) {
+    try {
+        // Отправляем сообщение всем подключенным WebSocket-клиентам
+        serverWS.clients.forEach((client) => {
+            if (client.readyState === ws.OPEN) {
+                const filteredMessage = {
+                    Position_latitude: message.Position_latitude,
+                    Position_longitude: message.Position_longitude,
+                    Callsign: message.Callsign,
+                    Hex: message.HEX,
+                    direction: message.Track
+                };
+                client.send(JSON.stringify(filteredMessage));
+                console.log('Message sent to WebSocket client:', filteredMessage);
+            }
+        });
+    } catch (error) {
+        console.error('Failed to send message to WebSocket:', error);
     }
 }
 
 // Маршрут для приёма сообщений
 app.post('/sendMessage', async (req, res) => {
     const message = req.body;
+    // console.log(req)
+    // console.log(req.body)
 
     if (!message) {
         return res.status(400).send('"message" is required in request body.');
     }
 
     await sendMessageToQueue(message);
+    await sendMessageToVue(message);
     res.send('Message sent to RabbitMQ.');
 });
 
--- one_page2/src/views/testMapPage.vue
+++ one_page2/src/views/testMapPage.vue
@@ -1,71 +1,227 @@
 <template>
     <div>
-        <b-button @click="sendMessage()">Send Message</b-button>
-        <div v-if="message">
-            <p>Received Message: {{ message }}</p>
-        </div>
+        <div id="map"></div>
     </div>
 </template>
 
 <script>
 /* eslint-disable */
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import * as turf from 'turf';
 
 export default {
     data() {
         return {
-            message: null,
-            data: {
-                queue: "to_django_requests",
-                message: "get_flight_data"
-            },
-            socketget: null,
-            socketsend: null,
+            socketgetplane: null,
+            map: null,
+            startCoordinates: [24.7, 56.9],
+            planeCollection: {},
         };
     },
+    mounted() {
+        this.map = new maplibregl.Map({
+            container: 'map',
+            style: 'https://tiles.openfreemap.org/styles/liberty',
+            center: this.startCoordinates,
+            zoom: 6.5,
+        });
+    },
     created() {
-        this.setupWebSocket();
+        this.setupWebSocketPlanes();
     },
     methods: {
-
-        setupWebSocket() {
-            this.socketget = new WebSocket('ws://localhost:8081');
-
-            this.socketget.onopen = () => {
-                console.log('WebSocket connected');
-            };
-
-
-            this.socketget.onmessage = (event) => {
-                const data = JSON.parse(event.data);
-                console.log('Received data:', data);
-                this.message = data.message;
-            };
-
-            this.socketget.onerror = (error) => {
-                console.error('WebSocket error:', error);
-            };
+        removePlaneFromCollection(callSign) {
+            this.$delete(this.planeCollection, callSign);
         },
 
+        addPlaneToCollection(callSign, plane) {
+            this.$set(this.planeCollection, callSign, plane);
+        },
 
-        sendMessage() {
-            fetch('http://localhost:8081/sendMessage', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    queue: this.data.queue, // Имя очереди
-                    message: this.data.message, // Сообщение
-                }),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Message sent:', data);
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
+        setupWebSocketPlanes() {
+            this.socketgetplane = new WebSocket('ws://localhost:8082');
+
+            this.socketgetplane.onopen = () => {
+                console.log('WebSocketGetPlane connected');
+            };
+
+            this.socketgetplane.onmessage = (event) => {
+                console.log('--------------------------------');
+                const data = JSON.parse(event.data);
+                console.log('Received plane:', data);
+                const position = [data.Position_latitude, data.Position_longitude];
+
+                if (this.planeCollection[data.Callsign]) {
+                    console.log(`Plane with callSign ${data.Callsign} already exists.`);
+                    this.planeCollection[data.Callsign].updatePosition(data.Position_longitude, data.Position_latitude, data.direction);
+                } else {
+                    // Создаём новый объект Plane и добавляем в коллекцию
+                    this.planeCollection[data.Callsign] = new Plane(
+                        data.Position_longitude,
+                        data.Position_latitude,
+                        data.Callsign,
+                        data.direction,
+                        this.map,
+                        this.removePlaneFromCollection,
+                    );
+                    console.log(`Plane ${data.Callsign} added to collection.`);
+                }
+            };
+
+            this.socketgetplane.onerror = (error) => {
+                console.error('WebSocketGetPlane error:', error);
+            };
         },
     },
 };
+
+class Plane {
+    constructor(longitude, latitude, callSign, rotation, map, removePlaneFromCollection) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.rotation = rotation;
+        this.callSign = callSign;
+        this.sourceId = `plane-source-${callSign}`;
+        this.layerId = `plane-layer-${callSign}`;
+        this.map = map;
+        this.deleteAfterSeconds = 15000;
+        this.timer = null;
+        this.removePlaneFromCollection = removePlaneFromCollection;
+
+        console.log(`${this.callSign} plane has been created.`);
+
+        //-------------------------------------------------------------------------------------------------------------------
+        const width = 8;
+        const bytesPerPixel = 4;
+        const data = new Uint8Array(width * width * bytesPerPixel);
+
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < width; y++) {
+                const offset = (y * width + x) * bytesPerPixel;
+                data[offset + 0] = (y / width) * 255;
+                data[offset + 1] = (x / width) * 255;
+                data[offset + 2] = 128;
+                data[offset + 3] = 255;
+            }
+        }
+
+        this.map.addImage(`plane-${callSign}`, { width, height: width, data });
+
+        this.map.addSource(this.sourceId, {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [this.longitude, this.latitude],
+                        },
+                        properties: {
+                            rotation: this.rotation,
+                        },
+                    },
+                ],
+            },
+        });
+
+        this.map.addLayer({
+            id: this.layerId,
+            type: 'symbol',
+            source: this.sourceId,
+            layout: {
+                'icon-image': `plane-${callSign}`,
+                'icon-rotate': ['get', 'rotation'],
+                'icon-size': 1.5,
+            },
+        });
+
+        this.startDeleteTimer();
+    }
+
+    updatePosition(newLongitude, newLatitude, newRotation) {
+        this.longitude = newLongitude;
+        this.latitude = newLatitude;
+        this.rotation = newRotation;
+
+        if (!this.map.getLayer(this.layerId)) {
+            this.map.addLayer({
+                id: this.layerId,
+                type: 'symbol',
+                source: this.sourceId,
+                layout: {
+                    'icon-image': `plane-${this.callSign}`,
+                    'icon-rotate': ['get', 'rotation'],
+                    'icon-size': 1.5,
+                },
+            });
+        }
+
+        if (this.map.getSource(this.sourceId)) {
+            this.map.getSource(this.sourceId).setData({
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [this.longitude, this.latitude],
+                        },
+                        properties: {
+                            rotation: this.rotation,
+                        },
+                    },
+                ],
+            });
+        }
+
+        this.map.on('click', this.layerId, (e) => {
+            const features = this.map.queryRenderedFeatures(e.point, {
+                layers: [this.layerId]
+            });
+
+            if (features.length > 0) {
+                const feature = features[0];
+                // Выводим сообщение при клике
+                console.log(`You clicked on plane with call sign: ${this.callSign}`);
+            }
+        });
+        //------------------------------------------------------------------------------------------------------
+        this.resetDeleteTimer();
+    }
+
+    startDeleteTimer() {
+        this.timer = setTimeout(() => {
+            console.log(`${this.callSign} not updated, removing from collection.`);
+            this.destroy();
+        }, this.deleteAfterSeconds);
+    }
+
+    resetDeleteTimer() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+        this.startDeleteTimer();
+    }
+
+    destroy() {
+        console.log(`${this.callSign} is being destroyed.`);
+        if (this.map.getLayer(this.layerId)) {
+            this.map.removeLayer(this.layerId);
+        }
+    }
+}
+
 </script>
+
+<style scoped>
+#map {
+    position: absolute;
+    top: 112px;
+    left: 0;
+    width: 100%;
+    height: calc(100% - 112px);
+}
+</style>
